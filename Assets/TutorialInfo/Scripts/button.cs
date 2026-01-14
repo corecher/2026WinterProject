3159425a6db24be5795a4787fdcb0af5be22e3bf
dlwogus-boy <dlwogus83@gmail.com>
2026-01-14 20:34:09 +0900

    gamegetinUI

---
+++ Assets/TutorialInfo/Scripts/button.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class button : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
+{
+    public float moveDistance = 10f;      
+    public float duration = 0.1f;        
+    public float colorBrighten = 1f;    
+
+    private RectTransform rect;
+    private Vector2 originalPos;
+    private Image image;
+    private Color originalColor;
+
+    private Coroutine currentCoroutine;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+
+        originalPos = rect.anchoredPosition;
+        originalColor = image.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartHover(originalPos + Vector2.up * moveDistance,
+                   originalColor * colorBrighten);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StartHover(originalPos, originalColor);
+    }
+
+    void StartHover(Vector2 targetPos, Color targetColor)
+    {
+        if (currentCoroutine != null)
+            StopCoroutine(currentCoroutine);
+
+        currentCoroutine = StartCoroutine(Animate(targetPos, targetColor));
+    }
+    public void ClearButtonSelection()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    IEnumerator Animate(Vector2 targetPos, Color targetColor)
+    {
+        Vector2 startPos = rect.anchoredPosition;
+        Color startColor = image.color;
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / duration;
+            rect.anchoredPosition = Vector2.Lerp(startPos, targetPos, t);
+            image.color = Color.Lerp(startColor, targetColor, t);
+            yield return null;
+        }
+    }
+}
