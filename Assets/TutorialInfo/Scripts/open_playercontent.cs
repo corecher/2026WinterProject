f73c374b4423e555d0554eb1491170dfa470b1b7
dlwogus-boy <dlwogus83@gmail.com>
2026-01-28 20:28:09 +0900

    dd

---
+++ Assets/TutorialInfo/Scripts/open_playercontent.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class open_playercontent : MonoBehaviour
+{
+    public GameObject player_content;
+    private bool Isopen = false;
+    
+    void Start()
+    {
+        Update_PlayerUI();
+    }
+
+    public void OnclickPlayerButton()
+    {
+        Isopen = !Isopen;
+        Update_PlayerUI();
+    }
+
+    void Update_PlayerUI()
+    {
+        if (Isopen)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    
+}
